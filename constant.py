6dc5ed188997547b21955f221a4345666ab74f36
Sonali Suri <sonali.suri158@gmail.com>
2020-11-09 02:36:00 -0800

    Experimentation with size

--- constant.py
+++ constant.py
@@ -13,43 +13,43 @@ REPLY = "Reply"
 #  node host and port
 # ====================
 host = 'ec2-54-227-2-149.compute-1.amazonaws.com'
-port = '8362'
+port = '8262'
 
 # ====================
 #  node host and port
 # ====================
 host_1 = 'ec2-54-226-195-115.compute-1.amazonaws.com'
-port_1 = '8358'
+port_1 = '8258'
 
 # ====================
 #  node host and port
 # ====================
 host_2 = 'ec2-18-234-141-77.compute-1.amazonaws.com'
-port_2 = '8357'
+port_2 = '8257'
 
 # ====================
 #  node host and port
 # ====================
 host_3 = 'ec2-184-72-207-159.compute-1.amazonaws.com'
-port_3 = '8356'
+port_3 = '8256'
 
 # ====================
 #  node host and port
 # ====================
 host_4 = 'ec2-18-205-244-82.compute-1.amazonaws.com'
-port_4 = '8355'
+port_4 = '8255'
 
 # ====================
 #  node host and port
 # ====================
 host_diagram = 'localhost'
-port_diagram = '8373'
+port_diagram = '8273'
 
 # ====================
 #  node host and port
 # ====================
 host_node = 'localhost'
-port_node = '8354'
+port_node = '8254'
 
 
 # ====================
@@ -89,3 +89,4 @@ def get_string(char_length):
     return data_string
 
 
+#### CTRL + C
