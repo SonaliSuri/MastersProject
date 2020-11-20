f182825e7dc95ac53795ebf642a4ace9c1ca3737
Sonali Suri <sonali.suri158@gmail.com>
2020-11-20 00:48:11 -0800

    increase size

--- t2.py
+++ t2.py
@@ -2,24 +2,31 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
-objects = ('Prepare', 'Commit', 'CommitACK','Reply','Total')
+objects = ('10000','100000','1048000','10230000','40000000','41300000')
 y_pos = np.arange(len(objects))
-performance = [0.026417255401611328*1000000, 0.021852970123291016*1000000, 0.03026723861694336*1000000, 0.05371904373168945*1000000, 132256.5078735351]
+performance = [6.899,68.70,722.80, 7066.89,28870.65, 29800.24]
 plt.bar(y_pos, performance, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Time')
 plt.title('us-east-1')
 plt.show()
 
+import matplotlib.pyplot as plt
+
+plt.plot(objects,performance)
+plt.title('Throuhput')
+plt.xlabel('Bytes/s')
+plt.ylabel('PPS')
+plt.show()
 
 """
-98257.06481933594
-118568.18199157715
-119150.16174316406
-134236.09733581543
-136705.3985595703
-173153.87725830078
-539921.9989776611
+
+
+
+
+
+
+
 
 1603168394.6725395
 1603168394.6862798
